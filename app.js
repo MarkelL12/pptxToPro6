4ba9271b0825030729f7309043b7cc37fe768846
MarkelL12 <mark.l.fsbc@gmail.com>
2024-06-19 19:51:18 -0500

    used office-text-extractor instead and extracted pptx text

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-import JSZip from 'jszip';
-import { DOMParser } from 'xlmdom'
+import { getTextExtractor } from 'office-text-extractor'
 
+const extractor = getTextExtractor()
+const path = './ppt.pptx'
+const text = await extractor.extractText({ input: path, type: 'file' })
+const textSlides = text.split("---");
+
+//todo convert each slide to plainText Base64 encoded
+//todo convert each slide to RTF and then to Base64 encoded
+
+//todo take Pro6 file template and insert slides
+
+console.log(slides)
